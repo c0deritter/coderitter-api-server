e8b54a269b95fde14e7afa50b9c72d83df30a564
R Gastmeier <r.gastmeier@gmail.com>
2020-01-04 16:45:21 +0100

    Add NatsApiConfig to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { default as Api } from './lib/api/Api'
-export { default as NatsApi } from './lib/api/NatsApi'
+export { default as NatsApi, NatsApiConfig } from './lib/api/NatsApi'
 export { default as NatsClient } from './lib/api/NatsClient'
 export { default as PostOnlyApi } from './lib/api/PostOnlyApi'
 export { default as StandardEndpoint } from './lib/api/StandardEndpoint'
