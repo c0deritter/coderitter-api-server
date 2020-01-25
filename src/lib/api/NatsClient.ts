88639ab2f49fb073a186f475c8c1470b33a765cb
R Gastmeier <r.gastmeier@gmail.com>
2020-01-25 21:13:21 +0100

    Rename options to parameter

--- src/lib/api/NatsClient.ts
+++ src/lib/api/NatsClient.ts
@@ -15,36 +15,32 @@ export default class NatsClient {
     this.instantiator = instantiator
   }
 
-  async request<T>(server: string, methodName: string, options?: any): Promise<Result<T>> {
+  async request<T>(server: string, methodName: string, parameter?: any): Promise<Result<T>> {
     let l = log.fn('request')
     l.debug('server =', server)
     l.debug('methodName =', methodName)
-    l.debug('options =', options)
+    l.debug('parameter =', parameter)
 
-    let remoteOptions = new RemoteMethodCall(methodName, options)
-    l.debug('remoteOptions =', remoteOptions)
+    let remoteMethodCall: RemoteMethodCall = { methodName: methodName, parameter: parameter }
+    l.debug('remoteMethodCall =', remoteMethodCall)
 
-    let remoteOptionsObj = toJsonObj(remoteOptions)
-    l.debug('remoteOptionsObj =', remoteOptionsObj)
+    let remoteMethodCallObj = toJsonObj(remoteMethodCall)
+    l.debug('remoteMethodCallObj =', remoteMethodCallObj)
 
-    let remoteOptionsJson = JSON.stringify(remoteOptionsObj)
-    l.insane('remoteOptionsJson =', remoteOptionsJson)
+    let remoteMethodCallJson = JSON.stringify(remoteMethodCallObj)
+    l.insane('remoteMethodCallJson =', remoteMethodCallJson)
 
-    let natsResult = await this.originalNatsClient.request(server, 1000, remoteOptionsJson)
+    let natsResult = await this.originalNatsClient.request(server, 1000, remoteMethodCallJson)
     l.debug('natsResult.data =', natsResult.data)
-    let resultObj = JSON.parse(natsResult.data)
-    l.debug('resultObj =', resultObj)
-    let mismatch = Result.isValid(resultObj)
-    l.debug('mismatch =', mismatch)
+    let resultData = JSON.parse(natsResult.data)
+    l.debug('resultData =', resultData)
 
-    if (mismatch == undefined) {
-      l.insane('this.instantiator =', this.instantiator)
-      let result = fromJsonObj(resultObj, this.instantiator)
-      l.debug('result =', result)
-      return <Result<T>> result
-    }
-    else {
-      throw new Error('Received result did not match the expected format ' + mismatch.message)
-    }
+    // throws error if not valid
+    Result.checkIfRawResultObjIsValid(resultData)
+
+    l.insane('this.instantiator =', this.instantiator)
+    let result = fromJsonObj(resultData, this.instantiator)
+    l.debug('result =', result)
+    return <Result<T>> result
   }
 }
